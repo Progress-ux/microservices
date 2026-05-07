3ec7c8016e8126079e662f63a6a5ae480da93ea8
progress <nikita15933@mail.ru>
2026-05-07 15:03:00 +0300

    implement registration endpoint and validation email

---
+++ src/main/java/com/progress/schoolmicroservices/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.progress.schoolmicroservices.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
