32998b7d5f3179b94b78d940065668c8ae58995c
progress <nikita15933@mail.ru>
2026-05-07 15:01:24 +0300

    add auth service and password encoder

---
+++ src/main/java/com/progress/schoolmicroservices/service/AuthService.java
@@ -0,0 +1,36 @@
+package com.progress.schoolmicroservices.service;
+
+import com.progress.schoolmicroservices.exception.EmailAlreadyExistsException;
+import com.progress.schoolmicroservices.model.dto.RegisterRequest;
+import com.progress.schoolmicroservices.model.entity.User;
+import com.progress.schoolmicroservices.model.enums.Role;
+import com.progress.schoolmicroservices.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    public void register(RegisterRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new EmailAlreadyExistsException("Пользователь с таким email уже существует");
+        }
+
+        User user = new User();
+
+        user.setEmail(request.getEmail().trim().toLowerCase());
+        user.setFirstName(request.getFirstName());
+        user.setMiddleName(request.getMiddleName());
+        user.setLastName(request.getLastName());
+        user.setRole(Role.STUDENT);
+
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepository.save(user);
+    }
+}
