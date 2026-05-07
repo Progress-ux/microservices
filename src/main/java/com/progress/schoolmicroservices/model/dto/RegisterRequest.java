d5faeb5b679a837d39c2e10010880fadcbda492f
progress <nikita15933@mail.ru>
2026-05-07 15:00:37 +0300

    add register request

---
+++ src/main/java/com/progress/schoolmicroservices/model/dto/RegisterRequest.java
@@ -0,0 +1,31 @@
+package com.progress.schoolmicroservices.model.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @Email()
+    @NotBlank(message = "Email не может быть пустым")
+    @Pattern(
+        regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
+        message = "Некорректный email"
+    )
+    private String email;
+
+    @NotBlank(message = "Пароль не может быть пустым")
+    @Size(min = 8, max = 64, message = "Минимум 8 символов")
+    private String password;
+
+    @NotBlank(message = "Поле 'Имя' не может быть пустым")
+    private String firstName;
+
+    private String middleName;
+
+    @NotBlank(message = "Поле 'Фамилия' не может быть пустым")
+    private String lastName;
+}
