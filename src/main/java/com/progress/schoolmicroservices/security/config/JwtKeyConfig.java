86a3cc6455a6d0369db7045c5cc0a45c8a793206
progress <nikita15933@mail.ru>
2026-05-13 19:55:40 +0300

    add public key

--- src/main/java/com/progress/schoolmicroservices/security/config/JwtKeyConfig.java
+++ src/main/java/com/progress/schoolmicroservices/security/config/JwtKeyConfig.java
@@ -3,7 +3,9 @@ package com.progress.schoolmicroservices.security.config;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -13,17 +15,17 @@ import org.springframework.core.io.Resource;
 @Configuration
 public class JwtKeyConfig {
    @Value("${jwt.private-key-path}")
-   private Resource keyResource;
-   
+   private Resource privateKeyResource;
+
+   @Value("${jwt.public-key-path}")
+   private Resource publicKeyResource;
+
    @Bean
    public PrivateKey privateKey() throws Exception {
-      byte[] keyBytes = keyResource.getInputStream().readAllBytes();
-      String tempKey = new String(keyBytes, StandardCharsets.UTF_8);
-
-      String privateKeyPEM = tempKey
-      .replace("-----BEGIN PRIVATE KEY-----", "")
-      .replace("-----END PRIVATE KEY-----", "")
-      .replaceAll("\\s", "");
+      String privateKeyPEM = new String(privateKeyResource.getInputStream().readAllBytes(), StandardCharsets.UTF_8)
+         .replace("-----BEGIN PRIVATE KEY-----", "")
+         .replace("-----END PRIVATE KEY-----", "")
+         .replaceAll("\\s", "");
 
       byte[] decoded = Base64.getDecoder().decode(privateKeyPEM);
       PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
@@ -32,4 +34,18 @@ public class JwtKeyConfig {
       return kf.generatePrivate(spec);
    }
 
+   @Bean
+   public PublicKey publicKey() throws Exception {
+      String publicKeyPEM = new String(publicKeyResource.getInputStream().readAllBytes(), StandardCharsets.UTF_8)
+         .replace("-----BEGIN PUBLIC KEY-----", "")
+         .replace("-----END PUBLIC KEY-----", "")
+         .replaceAll("\\s", "");
+
+
+      byte[] decoded = Base64.getDecoder().decode(publicKeyPEM);
+      X509EncodedKeySpec spec = new X509EncodedKeySpec(decoded); 
+      KeyFactory kf = KeyFactory.getInstance("RSA");
+
+      return kf.generatePublic(spec);
+   }
 }
--- src/main/java/com/progress/schoolmicroservices/security/jwt/JwtTokenService.java
+++ src/main/java/com/progress/schoolmicroservices/security/jwt/JwtTokenService.java
@@ -1,29 +1,45 @@
 package com.progress.schoolmicroservices.security.jwt;
 
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.Date;
 
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 
 @Component
 public class JwtTokenService {
 
    private final PrivateKey privateKey;
+   private final PublicKey publicKey;
 
    public JwtTokenService(
-      PrivateKey privateKey
+      PrivateKey privateKey,
+      PublicKey publicKey
    ) {
       this.privateKey = privateKey;
+      this.publicKey = publicKey;
    }
 
-   public String generateToken(String email) {
+   public String generateToken(String email, long duration, String tokenType) {
       return Jwts.builder()
          .subject(email)
+         .claim("token_type", tokenType)
          .issuedAt(new Date())
-         .expiration(new Date(System.currentTimeMillis() + 1000 * 60))
+         .expiration(new Date(System.currentTimeMillis() + duration))
          .signWith(privateKey)
          .compact();
    }
+
+   public Claims parseToken(String token) {
+      return Jwts.parser()
+         .verifyWith(publicKey)
+         .build()
+         .parseSignedClaims(token)
+         .getPayload();
+   }
+
+   public PublicKey getPublicKey() { return publicKey; }
 }
