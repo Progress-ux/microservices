a88f81c1fb231463ff9f52645520c1dee79de24c
progress <nikita15933@mail.ru>
2026-05-17 14:04:55 +0300

    add gateway properties

--- gateway-service/build.gradle.kts
+++ gateway-service/build.gradle.kts
@@ -5,8 +5,10 @@ plugins {
 description = "gateway-service"
 
 dependencies {
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webmvc")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     
     implementation(platform("io.jsonwebtoken:jjwt-bom:0.13.0"))
     implementation("io.jsonwebtoken:jjwt-api")
