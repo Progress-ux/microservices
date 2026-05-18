af8c7cc77d16435a90476df723de4f6ac8851205
progress <nikita15933@mail.ru>
2026-05-18 12:46:25 +0300

    remove jwt and lombok implementations

--- gateway-service/build.gradle.kts
+++ gateway-service/build.gradle.kts
@@ -9,12 +9,4 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-    
-    implementation(platform("io.jsonwebtoken:jjwt-bom:0.13.0"))
-    implementation("io.jsonwebtoken:jjwt-api")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson")
-
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
 }
