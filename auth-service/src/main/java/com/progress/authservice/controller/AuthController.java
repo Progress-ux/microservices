b1d0508c0f890e26307765a73a0c07627ba83c15
progress <nikita15933@mail.ru>
2026-05-19 11:40:12 +0300

    delete import

--- auth-service/src/main/java/com/progress/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/progress/authservice/controller/AuthController.java
@@ -11,8 +11,6 @@ import com.progress.authservice.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
