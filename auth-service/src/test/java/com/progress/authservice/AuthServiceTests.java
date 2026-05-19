29b3971a152be852e58a65108aa6c49b527973f6
progress <nikita15933@mail.ru>
2026-05-19 13:29:22 +0300

    add tests for refresh

--- auth-service/src/test/java/com/progress/authservice/AuthServiceTests.java
+++ auth-service/src/test/java/com/progress/authservice/AuthServiceTests.java
@@ -3,24 +3,31 @@ package com.progress.authservice;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
+import java.time.Duration;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.progress.authservice.exception.EmailAlreadyExistsException;
 import com.progress.authservice.exception.InvalidCredentialsException;
 import com.progress.authservice.model.dto.LoginRequest;
+import com.progress.authservice.model.dto.RefreshResponse;
 import com.progress.authservice.model.dto.RegisterRequest;
 import com.progress.authservice.model.entity.User;
 import com.progress.authservice.repository.UserRepository;
+import com.progress.authservice.security.jwt.JwtTokenService;
 import com.progress.authservice.service.AuthService;
+
+import io.jsonwebtoken.Claims;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -34,6 +41,15 @@ class AuthServiceTests {
 
    @Mock
    private PasswordEncoder passwordEncoder;  
+
+   @Mock
+   private JwtTokenService jwtTokenService;
+
+   @BeforeEach
+   void setUp() {
+      ReflectionTestUtils.setField(authService, "accessTokenLifeTime", Duration.ofMinutes(15));
+      ReflectionTestUtils.setField(authService, "refreshTokenLifeTime", Duration.ofDays(30));
+   }
 
    @Test
    void register_ShouldThrowException_WhenUserExists() {
@@ -83,5 +99,35 @@ class AuthServiceTests {
       assertThatThrownBy(() -> authService.login(request))
          .isInstanceOf(InvalidCredentialsException.class)
          .hasMessageContaining("Неверный email или пароль");
+   }
+
+   @Test
+   void refresh_ShouldReturnNewTokens_WhenRefreshTokenIsValid() {
+      String oldRefreshToken = "valid_refresh_token";
+      Claims claims = mock(Claims.class);
+      
+      when(jwtTokenService.parseToken(oldRefreshToken)).thenReturn(claims);
+      when(claims.get("token_type", String.class)).thenReturn("REFRESH");
+      when(claims.getSubject()).thenReturn("test@example.com");
+
+      when(jwtTokenService.generateToken(anyString(), anyLong(), eq("REFRESH"))).thenReturn("new_refresh_token");
+      when(jwtTokenService.generateToken(anyString(), anyLong(), eq("ACCESS"))).thenReturn("new_access_token");
+
+      RefreshResponse response = authService.refresh(oldRefreshToken);
+
+      assertThat(response.refreshToken()).isEqualTo("new_refresh_token");
+      assertThat(response.accessToken()).isEqualTo("new_access_token");
+   }
+
+   @Test
+   void refresh_ShouldThrow_WhenTokenInvalidOrExpired() {
+      // Given
+      String invalidToken = "invalid_token";
+
+      when(jwtTokenService.parseToken(invalidToken)).thenThrow(new RuntimeException("Expired"));
+
+      assertThatThrownBy(() -> authService.refresh(invalidToken))
+         .isInstanceOf(InvalidCredentialsException.class)
+         .hasMessageContaining("Токен обновления недействителен или просрочен");
    }
 }
